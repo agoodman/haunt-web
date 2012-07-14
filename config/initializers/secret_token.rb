9d97de5d7f577e62416495ea24eb34fddf5cbcac
Aubrey Goodman <aubrey.goodman@gmail.com>
2012-07-14 17:02:02 -0400

    basic rails app with README

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Haunt::Application.config.secret_token = 'ba83ef0ff9bf2375ec51f61ef25bce712fcdee711ab7d2abb053c260d9a818d252e2b116111edd636d33b00c1be9ab63c7753139ced0b85c8bbc282731b11f3d'
